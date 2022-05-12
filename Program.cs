d7567eaf407fdeccf73c701a67940b1255f2a8f4
sohaila-ashraf-albdAllah <suhayla.alkwady@gmail.com>
2022-05-12 22:08:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace N_Puzzel_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Puzzel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace N_Puzzel_Project
+{
+    class Puzzel
+    {
+    }
+}
