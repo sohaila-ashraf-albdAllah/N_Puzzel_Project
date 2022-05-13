b4a09126b7da29b27726927079637e30dde5108a
sohaila-ashraf-albdAllah <suhayla.alkwady@gmail.com>
2022-05-14 01:18:11 +0200

    edit

--- Priority_Queue.cs
+++ Priority_Queue.cs
@@ -7,18 +7,18 @@ namespace N_Puzzel_Project
     class Priority_Queue
     {
         
-        public List<puzzel> PUZZLE;
+        public List<Puzzel> PUZZLE;
         public Priority_Queue()
         {                                                                                                                                                                                                             
-            PUZZLE = new List<puzzel>();
+            PUZZLE = new List<Puzzel>();
         }
-        static void swap_puzzel(puzzel x, puzzel y)
+        static void swap_puzzel(Puzzel x, Puzzel y)
         {
-            puzzel tempswap = x;
+            Puzzel tempswap = x;
             x = y;
             y  = tempswap;
         }
-        public void Enqueue(puzzel item)
+        public void Enqueue(Puzzel item)
         {
             PUZZLE.Add(item);
             int child_index = PUZZLE.Count - 1, parent_index;
@@ -31,9 +31,9 @@ namespace N_Puzzel_Project
                 goto here;
             }
         }
-        public puzzel Dequeue()
+        public Puzzel Dequeue()
         {
-            puzzel frontItem = PUZZLE[0];
+            Puzzel frontItem = PUZZLE[0];
             int lastindex_beforeremove = PUZZLE.Count - 1;
             PUZZLE[0]= PUZZLE[lastindex_beforeremove];
             PUZZLE.RemoveAt(lastindex_beforeremove);
@@ -48,7 +48,7 @@ namespace N_Puzzel_Project
                     leftchild_index = right_children;
                 if (PUZZLE[parent_index].cost > (PUZZLE[leftchild_index].cost))
                 {
-                    puzzel tmp = PUZZLE[parent_index];
+                    Puzzel tmp = PUZZLE[parent_index];
                     PUZZLE[parent_index] = PUZZLE[leftchild_index];
                     PUZZLE[leftchild_index] = tmp;
                     parent_index = leftchild_index;
