75da4e2476cfeba1e3f1da840b6bc58024bad3d1
YasmineAhmedSaied <101922750+YasmineAhmedSaied@users.noreply.github.com>
2022-05-14 19:44:34 +0200

    yasmine

--- Priority_Queue.cs
+++ Priority_Queue.cs
@@ -5,18 +5,8 @@ namespace N_Puzzel_Project
 {
     class Priority_Queue
     {  
-        public List<Puzzel> PUZZLE;
-        public Priority_Queue()
-        {                                                                                                                                                                                                             
-            PUZZLE = new List<Puzzel>();
-        }
-        static void swap_puzzel(Puzzel x, Puzzel y)
-        {
-            Puzzel tempswap = x;
-            x = y;
-            y  = tempswap;
-        }
-        public void Enqueue(Puzzel item)
+        public List<puzzel> PUZZLE = new List<puzzel>();
+        public void Enqueue(puzzel item)
         {
             PUZZLE.Add(item);
             int child_index = PUZZLE.Count - 1, parent_index;
@@ -24,14 +14,16 @@ namespace N_Puzzel_Project
             parent_index = (child_index - 1) / 2;
             if ((PUZZLE[child_index].cost) < (PUZZLE[parent_index].cost))
             {
-                swap_puzzel(PUZZLE[child_index], PUZZLE[parent_index]);
+                puzzel tmp = PUZZLE[parent_index];
+                PUZZLE[parent_index] = PUZZLE[child_index];
+                PUZZLE[child_index] = tmp;
                 child_index = parent_index;
                 goto here;
             }
         }
-        public Puzzel Dequeue()
+        public puzzel Dequeue()
         {
-            Puzzel frontItem = PUZZLE[0];
+            puzzel frontItem = PUZZLE[0];
             int lastindex_beforeremove = PUZZLE.Count - 1;
             PUZZLE[0]= PUZZLE[lastindex_beforeremove];
             PUZZLE.RemoveAt(lastindex_beforeremove);
@@ -46,7 +38,7 @@ namespace N_Puzzel_Project
                     leftchild_index = right_children;
                 if (PUZZLE[parent_index].cost > (PUZZLE[leftchild_index].cost))
                 {
-                    Puzzel tmp = PUZZLE[parent_index];
+                    puzzel tmp = PUZZLE[parent_index];
                     PUZZLE[parent_index] = PUZZLE[leftchild_index];
                     PUZZLE[leftchild_index] = tmp;
                     parent_index = leftchild_index;
